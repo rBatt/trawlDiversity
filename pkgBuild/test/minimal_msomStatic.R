580a5f91fb464dbf630922bf1a8e2996935fbe85
rbatt <battrd@gmail.com>
2015-11-24 18:18:09 -0500

    add first example of static msom, all in 1 place
    See #99

---
+++ pkgBuild/test/minimal_msomStatic.R
@@ -0,0 +1,325 @@
+# ============
+# = Data Set =
+# ============
+staticData <- structure(list(X = structure(c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 
+0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 
+0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 
+0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 
+1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 
+0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 
+0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 
+1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 
+0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 
+0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0), .Dim = c(3L, 3L, 7L, 15L)), 
+    U = structure(c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1.3, 2.4, 3.9, 3.2, 3.2, 
+    3.5, 1.4, 2.6, 2.1, 1.1, 1.4, 4.1, 3.1, 3.2, 3.5, 0.6, 2.2, 
+    1.7, 1.9, 2.5, 3.1, 3.06, 2.8, 3.3, 0.4, 1.9, 1.7, 1.9, 2, 
+    3.1, 3.2, 3.15, 3.5, 0, 2.23333333333333, 0, 1.3, 1.2, 3.4, 
+    3, 3.4, 3.4, 0, 0, 0, 0, 0, 3.9, 2.8, 0, 0, 0, 0, 0, 0, 0, 
+    3.4, 0, 0, 0, 0, 0, 0, 1.69, 5.76, 15.21, 10.24, 10.24, 12.25, 
+    1.96, 6.76, 4.41, 1.21, 1.96, 16.81, 9.61, 10.24, 12.25, 
+    0.36, 4.84, 2.89, 3.61, 6.25, 9.61, 9.3636, 7.84, 10.89, 
+    0.16, 3.61, 2.89, 3.61, 4, 9.61, 10.24, 9.9225, 12.25, 0, 
+    4.98777777777778, 0, 1.69, 1.44, 11.56, 9, 11.56, 11.56, 
+    0, 0, 0, 0, 0, 15.21, 7.84, 0, 0, 0, 0, 0, 0, 0, 11.56, 0, 
+    0, 0, 0, 0, 0), .Dim = c(3L, 3L, 7L, 3L), .Dimnames = list(
+        c("2000", "2001", "2002"), c("-165.5 57.5", "-172.5 57.5", 
+        "-175.5 60.5"), c("1", "2", "3", "4", "5", "6", "7"), 
+        c("Intercept", "bt", "bt2"))), V = structure(c(1, 1, 
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
+    1, 1, 1, 1, -0.969346902235006, -1.30111741936901, -0.902992798808205, 
+    1.15398440742263, 0.556797476581418, 0.424089269727816, 0.755859786861821, 
+    1.02127620056903, 1.28669261427623, -0.969346902235006, -1.30111741936901, 
+    -0.902992798808205, 1.15398440742263, 0.556797476581418, 
+    0.424089269727816, 0.755859786861821, 1.08763030399583, 1.28669261427623, 
+    -0.969346902235006, -1.10205510908861, -1.10205510908861, 
+    1.15398440742263, 0.556797476581418, 0.357735166301015, 0.68950568343502, 
+    1.08763030399583, 1.22033851084943, -0.969346902235006, -1.10205510908861, 
+    -1.10205510908861, 1.35304671770303, 0.490443373154617, 0.357735166301015, 
+    0, 1.08763030399583, 0, -0.969346902235006, -1.10205510908861, 
+    -0.969346902235006, 1.28669261427623, 0.556797476581418, 
+    0.357735166301015, 0, 0, 0, 0, 0, -0.969346902235006, 1.28669261427623, 
+    0, 0, 0, 0, 0, 0, 0, -0.969346902235006, 0, 0, 0, 0, 0, 0, 
+    0.939633416872603, 1.69290653898548, 0.815395994699476, 1.33168001257455, 
+    0.310023429927435, 0.179851708698272, 0.571324017394798, 
+    1.0430050778487, 1.655577883633, 0.939633416872603, 1.69290653898548, 
+    0.815395994699476, 1.33168001257455, 0.310023429927435, 0.179851708698272, 
+    0.571324017394798, 1.18293967817005, 1.655577883633, 0.939633416872603, 
+    1.2145254634683, 1.2145254634683, 1.33168001257455, 0.310023429927435, 
+    0.127974449208415, 0.475418087489194, 1.18293967817005, 1.4892260810622, 
+    0.939633416872603, 1.2145254634683, 1.2145254634683, 1.83073542028695, 
+    0.240534702271279, 0.127974449208415, 0, 1.18293967817005, 
+    0, 0.939633416872603, 1.2145254634683, 0.939633416872603, 
+    1.655577883633, 0.310023429927435, 0.127974449208415, 0, 
+    0, 0, 0, 0, 0.939633416872603, 1.655577883633, 0, 0, 0, 0, 
+    0, 0, 0, 0.939633416872603, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 
+    1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 
+    0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+    0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 
+    1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 
+    0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 
+    0, 1, 0, 0, 0, 0, 0, 0), .Dim = c(3L, 3L, 7L, 5L), .Dimnames = list(
+        c("2000", "2001", "2002"), c("-165.5 57.5", "-172.5 57.5", 
+        "-175.5 60.5"), c("1", "2", "3", "4", "5", "6", "7"), 
+        c("Intercept", "doy", "doy2", "yr2001", "yr2002"))), 
+    nU = 3L, nV = 5L, nK = structure(c(5, 5, 7, 6, 5, 5, 3, 4, 
+    3), .Dim = c(3L, 3L), .Dimnames = structure(list(year = c("2000", 
+    "2001", "2002"), stratum = c("-165.5 57.5", "-172.5 57.5", 
+    "-175.5 60.5")), .Names = c("year", "stratum"))), nT = 3L, 
+    Jmax = 3L, Kmax = 7L, N = 5L, nS = 15), .Names = c("X", "U", 
+"V", "nU", "nV", "nK", "nT", "Jmax", "Kmax", "N", "nS"))
+
+
+# ==============
+# = Stan Model =
+# ==============
+model_file="
+// ===============
+// = Conventions =
+// ===============
+// Data Structures:
+	// Arrays: last index is fastest (most specific, smallest)
+	// Matrices: first index (row) is fastest
+// Notation:
+	// Scalar capital letters are the maximum value for their lowercase counterpart (t = 1, 2, ..., T)
+	// Greek letters are parameters
+	// State variables and covariates are capital Roman letters (X, Z, Y, U)
+
+// =============
+// = Functions =
+// =============
+functions {
+	
+	// log probability functions
+	
+	// lp for species that are observed
+	real lp_obs(int x, real logit_psi, real logit_theta) {
+		// could probably just drop x; 
+		// taken from function that used binomial		
+		return log_inv_logit(logit_psi) + bernoulli_logit_log(x, logit_theta);
+	}
+	
+	// lp for species that aren't observed, but known to exist
+	real lp_unobs(real logit_psi, real logit_theta) {
+		return log_sum_exp(lp_obs(0, logit_psi, logit_theta), log1m_inv_logit(logit_psi));
+	}
+	
+	real lp_never_obs(real logit_psi, real logit_theta, real Omega) {
+		real lp_unavailable;
+		real lp_available;
+		lp_unavailable <- bernoulli_log(0, Omega);
+		lp_available <- bernoulli_log(1, Omega) + lp_unobs(logit_psi, logit_theta);
+		return log_sum_exp(lp_unavailable, lp_available);
+	}
+	
+}
+
+
+// ========
+// = Data =
+// ========
+data {
+    int<lower=1> nT; // number of years
+    int<lower=1> Kmax; // maximum number of samples within a site
+    int<lower=1> Jmax; // number of sites
+    int<lower=0, upper=Kmax> nK[nT,Jmax]; // number of 'replicates' (hauls)
+    int nU; // number of Covariates for psi (presence)
+    int nV; // number of Covariates for theta (detection)
+    
+    matrix[Kmax,nU] U[nT,Jmax]; // covariates for psi (presence)
+    matrix[Kmax,nV] V[nT,Jmax]; // covariates for theta (detectability)
+    
+    int N; // total number of observed species (anywhere, ever)
+	int<lower=1> nS; // size of super population, includes unknown species
+    int X[nT,Jmax,Kmax,nS]; // species observations
+}
+
+
+// ====================
+// = Transformed Data =
+// ====================
+// transformed data {
+// 	int n0;
+//
+// 	n0 <- nS - N
+// }
+
+
+// ==============
+// = Parameters =
+// ==============
+parameters {	
+	real<lower=0, upper=1> Omega[nT]; // average availability
+	
+	vector[nU] alpha_mu; // hyperparameter mean
+	vector<lower=0>[nU] alpha_sd; // hyperparameter sd
+	matrix[nU,nS] alpha; // presence coefficient
+	
+	vector[nV] beta_mu; // hyperparameter mean
+	vector<lower=0>[nV] beta_sd; // hyperparameter sd
+	matrix[nV,nS] beta; // detection coefficient
+	
+}
+
+
+// ==========================
+// = Transformed Parameters =
+// ==========================
+transformed parameters {
+	row_vector[nS] logit_psi[nT,Jmax,Kmax]; // presence probability
+	row_vector[nS] logit_theta[nT,Jmax,Kmax]; // detection probability
+	
+	
+	for(t in 1:nT){
+		for(j in 1:Jmax){
+			int K;
+			K <- nK[t,j];
+			if(K>0){ // if K is not 0
+				for(k in 1:K){
+					logit_psi[t,j,k] <- U[t,j,k]*alpha;
+					logit_theta[t,j,k] <- V[t,j,k]*beta;
+				}
+				
+			}
+		}
+	}
+	
+}
+
+
+// =========
+// = Model =
+// =========
+model {
+	// Priors for hyperparameters
+	alpha_mu ~ cauchy(0, 2.5);
+	alpha_sd ~ cauchy(0, 2.5);
+	beta_mu ~ cauchy(0, 2.5);
+	beta_sd ~ cauchy(0, 2.5);
+	
+	Omega ~ beta(2,2);
+	
+	
+	// Priors for parameters
+	for(s in 1:nS){
+		for(u in 1:nU){
+			alpha[u,s] ~ normal(alpha_mu[u], alpha_sd[u]);
+		}
+		for(v in 1:nV){
+			beta[v,s] ~ normal(beta_mu[v], beta_sd[v]);
+		}
+	}
+	
+	
+	// ============================================
+	// = Begin Looping down to Point Observations =
+	// ============================================
+	for(t in 1:nT){ // loop through years
+		for(j in 1:Jmax){ // loop through sites
+			
+			// Define variable for number of sampling events
+			// (at this site, during this year)
+			int tK;
+			tK <- nK[t,j];
+			
+			if(tK>0){ // if >0 sampling events, proceed
+				for(k in 1:tK){// loop through sampling events
+					
+					
+					// =====================================
+					// = Two Species Loops for Likelihoods =
+					// =====================================
+					
+					// 1)
+					for(n in 1:N){// loop through observed species
+						1 ~ bernoulli(Omega[t]); // observed, so available
+						if(X[t,j,k,n] > 0){ // if present
+							// increment likelihood
+							increment_log_prob(
+								lp_obs(X[t,j,k,n], logit_psi[t,j,k,n], logit_theta[t,j,k,n])
+							);
+						}else{ // if absent
+							// increment likelihood
+							increment_log_prob(
+								lp_unobs(logit_psi[t,j,k,n], logit_theta[t,j,k,n])
+							);
+						}
+					} // end first species loop
+					
+					// 2)
+					for(s in (N+1):nS){ // loop through padded species
+						// increment likelihood
+						increment_log_prob(
+							lp_never_obs(logit_psi[t,j,k,s], logit_theta[t,j,k,s], Omega[t])
+						);
+					} // end second species loop
+					
+				} // end sample loop
+			} // if 0 samples, do nothing
+		} // end site loop
+	} // end year loop
+	
+	
+    
+} // end model
+
+
+// ========================
+// = Generated Quantities =
+// ========================
+// generated quantities {
+//
+// }
+"
+
+# =============
+# = Fit Model =
+# =============
+ebs_msom <- stan(
+	model_code=model_file, 
+	data=staticData, 
+	control=list(stepsize=0.05, adapt_delta=0.95),
+	chains=4, iter=500, refresh=1, seed=1337, cores=4
+)
