5446d5c11d48f7a327ccda287458c51630f292c9
rbatt <battrd@gmail.com>
2015-11-25 21:22:41 -0500

    update minimal example to share #99

--- pkgBuild/test/minimal_msomStatic.R
+++ pkgBuild/test/minimal_msomStatic.R
@@ -47,28 +47,17 @@ staticData <- structure(list(X = structure(c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0), .Dim = c(3L, 3L, 7L, 15L)), 
-    U = structure(c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1.3, 2.4, 3.9, 3.2, 3.2, 
-    3.5, 1.4, 2.6, 2.1, 1.1, 1.4, 4.1, 3.1, 3.2, 3.5, 0.6, 2.2, 
-    1.7, 1.9, 2.5, 3.1, 3.06, 2.8, 3.3, 0.4, 1.9, 1.7, 1.9, 2, 
-    3.1, 3.2, 3.15, 3.5, 0, 2.23333333333333, 0, 1.3, 1.2, 3.4, 
-    3, 3.4, 3.4, 0, 0, 0, 0, 0, 3.9, 2.8, 0, 0, 0, 0, 0, 0, 0, 
-    3.4, 0, 0, 0, 0, 0, 0, 1.69, 5.76, 15.21, 10.24, 10.24, 12.25, 
-    1.96, 6.76, 4.41, 1.21, 1.96, 16.81, 9.61, 10.24, 12.25, 
-    0.36, 4.84, 2.89, 3.61, 6.25, 9.61, 9.3636, 7.84, 10.89, 
-    0.16, 3.61, 2.89, 3.61, 4, 9.61, 10.24, 9.9225, 12.25, 0, 
-    4.98777777777778, 0, 1.69, 1.44, 11.56, 9, 11.56, 11.56, 
-    0, 0, 0, 0, 0, 15.21, 7.84, 0, 0, 0, 0, 0, 0, 0, 11.56, 0, 
-    0, 0, 0, 0, 0), .Dim = c(3L, 3L, 7L, 3L), .Dimnames = list(
+    U = structure(c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1.5, 1.9, 3.55714285714286, 
+    3.04666666666667, 3.1375, 3.43076923076923, 0.757142857142857, 
+    2.23333333333333, 1.83333333333333, 2.25, 3.61, 12.6532653061224, 
+    9.28217777777778, 9.84390625, 11.7701775147929, 0.573265306122449, 
+    4.98777777777778, 3.36111111111111), .Dim = c(3L, 3L, 3L), .Dimnames = list(
         c("2000", "2001", "2002"), c("-165.5 57.5", "-172.5 57.5", 
-        "-175.5 60.5"), c("1", "2", "3", "4", "5", "6", "7"), 
-        c("Intercept", "bt", "bt2"))), V = structure(c(1, 1, 
+        "-175.5 60.5"), c("Intercept", "bt", "bt2"))), V = structure(c(1, 
     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 
-    1, 1, 1, 1, -0.969346902235006, -1.30111741936901, -0.902992798808205, 
+    1, 1, 1, 1, 1, -0.969346902235006, -1.30111741936901, -0.902992798808205, 
     1.15398440742263, 0.556797476581418, 0.424089269727816, 0.755859786861821, 
     1.02127620056903, 1.28669261427623, -0.969346902235006, -1.30111741936901, 
     -0.902992798808205, 1.15398440742263, 0.556797476581418, 
@@ -108,37 +97,39 @@ staticData <- structure(list(X = structure(c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
     3), .Dim = c(3L, 3L), .Dimnames = structure(list(year = c("2000", 
     "2001", "2002"), stratum = c("-165.5 57.5", "-172.5 57.5", 
     "-175.5 60.5")), .Names = c("year", "stratum"))), nT = 3L, 
-    Jmax = 3L, Kmax = 7L, N = 5L, nS = 15), .Names = c("X", "U", 
-"V", "nU", "nV", "nK", "nT", "Jmax", "Kmax", "N", "nS"))
+    Jmax = 3L, Kmax = 7L, N = 5L, nS = 15, isUnobs = structure(c(0L, 
+    0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 
+    0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 
+    0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 1L, 0L, 0L, 0L, 1L, 
+    1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 
+    1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 
+    1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 
+    1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 
+    1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 
+    1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L), .Dim = c(3L, 
+    3L, 15L)), nJ = structure(c(3L, 3L, 3L), .Names = c("2000", 
+    "2001", "2002"))), .Names = c("X", "U", "V", "nU", "nV", 
+"nK", "nT", "Jmax", "Kmax", "N", "nS", "isUnobs", "nJ"))
 
 
 # ==============
 # = Stan Model =
 # ==============
 model_file="
-// ===============
-// = Conventions =
-// ===============
-// Data Structures:
-	// Arrays: last index is fastest (most specific, smallest)
-	// Matrices: first index (row) is fastest
-// Notation:
-	// Scalar capital letters are the maximum value for their lowercase counterpart (t = 1, 2, ..., T)
-	// Greek letters are parameters
-	// State variables and covariates are capital Roman letters (X, Z, Y, U)
-
 // =============
 // = Functions =
 // =============
 functions {
 	
-	// log probability functions
+	// ---- log probability functions ----
 	
 	// lp for species that are observed
 	real lp_obs(int x, real logit_psi, real logit_theta) {
-		// could probably just drop x; 
-		// taken from function that used binomial		
 		return log_inv_logit(logit_psi) + bernoulli_logit_log(x, logit_theta);
+	}
+	
+	vector lp_obs2(int[] x, vector lil_lp, row_vector logit_theta) {
+		return lil_lp + bernoulli_logit_log(x, logit_theta);
 	}
 	
 	// lp for species that aren't observed, but known to exist
@@ -146,6 +137,15 @@ functions {
 		return log_sum_exp(lp_obs(0, logit_psi, logit_theta), log1m_inv_logit(logit_psi));
 	}
 	
+	// lp that works as either lp_obs or lp_unobs, depending on isUnobs values
+	real lp_exists(int[] x, row_vector logit_psi, row_vector logit_theta, vector isUnobs, vector lil_lp) {
+		// FYI: both isUnobs and x are from X[t,j], 
+		// isUnobs is !max() swept out across X[t,j,1:K,n] for 1:N, x is just X[t,j,k,1:N]
+		// Intention is that isUnobs will be 0 for each x that is observed for any k.
+		return log_sum_exp(log_sum_exp(lp_obs2(x, lil_lp, logit_theta)), log_sum_exp(lil_lp .* isUnobs));
+	}
+	
+	// lp for species that were never observed
 	real lp_never_obs(real logit_psi, real logit_theta, real Omega) {
 		real lp_unavailable;
 		real lp_available;
@@ -162,17 +162,20 @@ functions {
 // ========
 data {
     int<lower=1> nT; // number of years
-    int<lower=1> Kmax; // maximum number of samples within a site
+    int<lower=1> Kmax; // max samples in a site-year
     int<lower=1> Jmax; // number of sites
-    int<lower=0, upper=Kmax> nK[nT,Jmax]; // number of 'replicates' (hauls)
+		int<lower=0, upper=Jmax> nJ[nT]; // number of sites in each year
+    int<lower=0, upper=Kmax> nK[nT,Jmax]; // number of samples (replicates/ hauls)
     int nU; // number of Covariates for psi (presence)
     int nV; // number of Covariates for theta (detection)
     
-    matrix[Kmax,nU] U[nT,Jmax]; // covariates for psi (presence)
+		int N; // total number of observed species (anywhere, ever)
+		int<lower=1> nS; // size of super population, includes unknown species
+		vector[nS] isUnobs[nT,Jmax]; // was a species unobserved (across all K) each site-year?
+    
+    matrix[Jmax,nU] U[nT]; // covariates for psi (presence)
     matrix[Kmax,nV] V[nT,Jmax]; // covariates for theta (detectability)
     
-    int N; // total number of observed species (anywhere, ever)
-	int<lower=1> nS; // size of super population, includes unknown species
     int X[nT,Jmax,Kmax,nS]; // species observations
 }
 
@@ -181,9 +184,7 @@ data {
 // = Transformed Data =
 // ====================
 // transformed data {
-// 	int n0;
-//
-// 	n0 <- nS - N
+// 
 // }
 
 
@@ -200,33 +201,15 @@ parameters {
 	vector[nV] beta_mu; // hyperparameter mean
 	vector<lower=0>[nV] beta_sd; // hyperparameter sd
 	matrix[nV,nS] beta; // detection coefficient
-	
 }
 
 
 // ==========================
 // = Transformed Parameters =
 // ==========================
-transformed parameters {
-	row_vector[nS] logit_psi[nT,Jmax,Kmax]; // presence probability
-	row_vector[nS] logit_theta[nT,Jmax,Kmax]; // detection probability
-	
-	
-	for(t in 1:nT){
-		for(j in 1:Jmax){
-			int K;
-			K <- nK[t,j];
-			if(K>0){ // if K is not 0
-				for(k in 1:K){
-					logit_psi[t,j,k] <- U[t,j,k]*alpha;
-					logit_theta[t,j,k] <- V[t,j,k]*beta;
-				}
-				
-			}
-		}
-	}
-	
-}
+// transformed parameters {
+// 
+// }
 
 
 // =========
@@ -234,75 +217,64 @@ transformed parameters {
 // =========
 model {
 	// Priors for hyperparameters
-	alpha_mu ~ cauchy(0, 2.5);
-	alpha_sd ~ cauchy(0, 2.5);
-	beta_mu ~ cauchy(0, 2.5);
-	beta_sd ~ cauchy(0, 2.5);
+	alpha_mu ~ cauchy(0, 1);
+	alpha_sd ~ cauchy(0, 2);
+	beta_mu ~ cauchy(0, 1);
+	beta_sd ~ cauchy(0, 2);
 	
 	Omega ~ beta(2,2);
 	
-	
 	// Priors for parameters
-	for(s in 1:nS){
-		for(u in 1:nU){
-			alpha[u,s] ~ normal(alpha_mu[u], alpha_sd[u]);
-		}
-		for(v in 1:nV){
-			beta[v,s] ~ normal(beta_mu[v], beta_sd[v]);
-		}
+	for (u in 1:nU) {
+		alpha[u] ~ normal(alpha_mu[u], alpha_sd[u]);
+	}
+	for (v in 1:nV) {
+		beta[v] ~ normal(beta_mu[v], beta_sd[v]);
 	}
 	
 	
-	// ============================================
-	// = Begin Looping down to Point Observations =
-	// ============================================
-	for(t in 1:nT){ // loop through years
-		for(j in 1:Jmax){ // loop through sites
+	// ---- Begin Looping down to Point Observations ----
+	for (t in 1:nT) { // loop through years
+		increment_log_prob(bernoulli_log(1, Omega[t]) * N * sum(nK[t])); // observed, so available
+		
+		if(nJ[t]){
+			matrix[nJ[t],nS] logit_psi; // presence probability
+			logit_psi <- block(U[t], 1, 1, nJ[t], nU)*alpha; // block matrix algebra for speed
+		
+			for (j in 1:Jmax) { // sites
 			
-			// Define variable for number of sampling events
-			// (at this site, during this year)
-			int tK;
-			tK <- nK[t,j];
-			
-			if(tK>0){ // if >0 sampling events, proceed
-				for(k in 1:tK){// loop through sampling events
+				if(nK[t,j]){ // if samples in site
 					
+					row_vector[N] t_logit_psi;
+					vector[N] lil_lp;
+					matrix[nK[t,j],nS] logit_theta; // detection probability
 					
-					// =====================================
-					// = Two Species Loops for Likelihoods =
-					// =====================================
+					t_logit_psi <- sub_row(logit_psi, j, 1, N);
+					for (nn in 1:N)
+						lil_lp[nn] <- log1m_inv_logit(t_logit_psi[nn]);
+					logit_theta <- block(V[t,j], 1, 1, nK[t,j], nV)*beta;
 					
-					// 1)
-					for(n in 1:N){// loop through observed species
-						1 ~ bernoulli(Omega[t]); // observed, so available
-						if(X[t,j,k,n] > 0){ // if present
-							// increment likelihood
-							increment_log_prob(
-								lp_obs(X[t,j,k,n], logit_psi[t,j,k,n], logit_theta[t,j,k,n])
-							);
-						}else{ // if absent
-							// increment likelihood
-							increment_log_prob(
-								lp_unobs(logit_psi[t,j,k,n], logit_theta[t,j,k,n])
-							);
-						}
-					} // end first species loop
-					
-					// 2)
-					for(s in (N+1):nS){ // loop through padded species
-						// increment likelihood
-						increment_log_prob(
-							lp_never_obs(logit_psi[t,j,k,s], logit_theta[t,j,k,s], Omega[t])
-						);
-					} // end second species loop
-					
-				} // end sample loop
-			} // if 0 samples, do nothing
-		} // end site loop
+					for (k in 1:nK[t,j]) {// sampling events
+						
+						// lp for species that have been observed at some point
+						increment_log_prob(lp_exists(
+							segment(X[t,j,k], 1, N), 
+							t_logit_psi,
+							sub_row(logit_theta, k, 1, N),
+							segment(isUnobs[t,j], 1, N),
+							lil_lp
+						));
+				
+						for (s in (N+1):nS) { // padded species
+							// increment_log_prob(lp_never_obs(logit_psi[t,j,k,s], logit_theta[t,j,k,s], Omega[t]));
+							increment_log_prob(lp_never_obs(logit_psi[j,s], logit_theta[k,s], Omega[t]));
+						} // end second species loop
+					} // end sample loop
+				} // if nK
+			} // end site loop
+		} // if nJ
 	} // end year loop
 	
-	
-    
 } // end model
 
 
@@ -314,6 +286,7 @@ model {
 // }
 "
 
+
 # =============
 # = Fit Model =
 # =============
@@ -321,5 +294,15 @@ ebs_msom <- stan(
 	model_code=model_file, 
 	data=staticData, 
 	control=list(stepsize=0.05, adapt_delta=0.95),
-	chains=4, iter=500, refresh=1, seed=1337, cores=4
+	chains=4, iter=50, refresh=1, seed=1337, cores=4, verbose=FALSE
 )
+
+
+# =========================
+# = Timing and Efficiency =
+# =========================
+(timing <- cbind(get_elapsed_time(ebs_msom), rowSums(get_elapsed_time(ebs_msom))))
+(max_time <- max(timing))
+(neff_mu <- mean(summary(ebs_msom)$summary[,"n_eff"]))
+(neff_sd <- sd(summary(ebs_msom)$summary[,"n_eff"]))
+max_time/neff_mu
