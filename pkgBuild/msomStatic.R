80bb2ea455463f638f4815d22ed7cf3fc999adc5
rbatt <battrd@gmail.com>
2015-11-30 18:33:34 -0500

    new options

--- pkgBuild/msomStatic.R
+++ pkgBuild/msomStatic.R
@@ -11,20 +11,20 @@ library("trawlDiversity")
 # = Subset Data to Use =
 # ======================
 # smallest data set
-ebs.a2 <- ebs.a[,list(year=year, spp=spp, stratum=stratum, K=K, abund=abund, btemp=btemp, doy=yday(datetime))]
+# ebs.a2 <- ebs.a[,list(year=year, spp=spp, stratum=stratum, K=K, abund=abund, btemp=btemp, doy=yday(datetime))]
 
 # largest data set
-# ebs.a2 <- ebs.agg2[,list(year=year, spp=spp, stratum=stratum, K=K, abund=abund, btemp=btemp, doy=yday(datetime))]
+ebs.a2 <- ebs.agg2[,list(year=year, spp=spp, stratum=stratum, K=K, abund=abund, btemp=btemp, doy=yday(datetime))]
 
 # medium data set
 # set.seed(1337)
-# ind <- mpick(ebs.agg2, p=c(stratum=50, year=10), weight=TRUE, limit=60)
+# ind <- mpick(ebs.agg2, p=c(stratum=30, year=10), weight=TRUE, limit=60)
 # logic <- expression(
 # 	spp%in%spp[ind]
 # 	& stratum%in%stratum[ind]
 # 	& year%in%year[ind]
 # )
-# ebs.a1 <- ebs.agg2[eval(logic)][pick(spp, 100, w=TRUE)]
+# ebs.a1 <- ebs.agg2[eval(logic)][pick(spp, 30, w=TRUE)]
 # ebs.a2 <- ebs.a1[,list(year=year, spp=spp, stratum=stratum, K=K, abund=abund, btemp=btemp, doy=yday(datetime))]
 
 
@@ -33,7 +33,7 @@ ebs.a2 <- ebs.a[,list(year=year, spp=spp, stratum=stratum, K=K, abund=abund, bte
 # ======================================
 # rename columns for shorthand
 setnames(ebs.a2, c("btemp"), c("bt"))
-ebs.a2[,yr:=year]
+ebs.a2[,yr:=scale(as.integer(year))]
 
 # aggregate and transform (^2) btemp
 mk_cov_rv_pow(ebs.a2, "bt", across="K", by=c("stratum","year"), pow=2)
@@ -49,7 +49,7 @@ mk_cov_rv(ebs.a2, "doy", across="K", by=c("stratum","year"))
 # = Cast Data for Stan =
 # ======================
 # ---- Get Basic Structure of MSOM Data Input ----
-staticData <- msomData(Data=ebs.a2, n0=15, cov.vars=c(bt="bt", bt2="bt2",doy="doy",yr="yr"), u.form=~bt+bt2, v.form=~doy, valueName="abund", cov.by=c("year","stratum"))
+staticData <- msomData(Data=ebs.a2, n0=100, cov.vars=c(bt="bt", bt2="bt2",doy="doy",yr="yr"), u.form=~bt+bt2, v.form=~doy, valueName="abund", cov.by=c("year","stratum"))
 
 staticData_sd <- msomData(Data=ebs.a2, n0=1, cov.vars=c(bt_sd="bt_sd",doy_sd="doy_sd",bt2_sd="bt2_sd"), u.form=~bt_sd+bt2_sd-1, v.form=~doy_sd-1, valueName="abund", cov.by=c("year","stratum"))[c("U","V")]
 
@@ -131,8 +131,8 @@ model_file <- "trawl/trawlDiversity/inst/stan/msomStatic.stan"
 ebs_msom <- stan(
 	file=model_file, 
 	data=staticData, 
-	control=list(stepsize=0.01, adapt_delta=0.95, max_treedepth=15),
-	chains=4, iter=100, refresh=1, seed=1337, cores=4, verbose=F
+	control=list(stepsize=0.01, adapt_delta=0.93, max_treedepth=15),
+	chains=4, iter=200, refresh=4, seed=1337, cores=4, verbose=F
 )
 
 
@@ -182,24 +182,32 @@ traceplot(ebs_msom, "lp__", window=c(1,50), inc_warmup=T)
 # = Parameter Summary =
 # =====================
 # what is the distribution of omega?
+par(mfrow=c(2,1), mar=c(2.5,2.5,0.1,0.1), mgp=c(1,0.1,0), tcl=-0.1, ps=9)
+png("~/Desktop/omega_full.png", width=3.5, height=3.5, units="in", res=150)
 Omega <- rstan::extract(ebs_msom, "Omega")[[1]]
 plot(density(Omega, from=0, to=1))
 omega_prior_q <- seq(0,1,length.out=length(Omega))
 omega_prior <- dbeta(omega_prior_q, 2, 2)
 lines(omega_prior_q, omega_prior, col="blue")
-
+dev.off()
 
 sims <- rstan::extract(ebs_msom)
 psi_mean <- plogis(apply(sims$logit_psi, 2:4, mean))
 theta_mean <- plogis(apply(sims$logit_theta, 2:4, mean))
 
-par(mfrow=c(2,1))
+png("~/Desktop/psi_theta_problem_full.png", width=3.5, height=6, units='in', res=150)
+par(mfrow=c(2,1), mar=c(2.5,2.5,0.1,0.1), mgp=c(1,0.1,0), tcl=-0.1, ps=9)
 hist(psi_mean)
 hist(theta_mean)
+dev.off()
 
 
 psi <- data.table(reshape2::melt(psi_mean, varnames=c("year","site","spp"), value.name="psi"), key=c("year","site","spp"))
 
+
+# ---- Psi and Theta Response Curves ----
+png("~/Desktop/psi_theta_responseCurves_full.png", width=3.5, height=6, units="in", res=150)
+par(mfrow=c(2,1), mar=c(2.5,2.5,0.1,0.1), mgp=c(1,0.1,0), tcl=-0.1, ps=9)
 # ---- Species Response Curves via Psi and bt ----
 alpha <- apply(sims$alpha, 2:3, mean)
 U <- apply(staticData$U, 3, function(x)seq(min(x), max(x), length.out=100))
@@ -224,8 +232,9 @@ for(i in 2:ncol(theta_resp)){
 }
 par(new=T)
 plot(density(staticData$V[,,"doy"]), xaxt="n",yaxt="n", ylab="",xlab="", main="",type="l", col="blue")
+dev.off()
 
-
-
+# ---- save ----
+save.image("trawl/trawlDiversity/pkgBuild/test/msomStatic_full.RData")
 
 
