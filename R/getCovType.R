3159ce4e1468a634a56875158fc35b806ebc7c46
rBatt <battrd@gmail.com>
2015-12-02 22:16:16 -0500

    add getCovType as function

---
+++ R/getCovType.R
@@ -0,0 +1,52 @@
+#' Get Covariate Type
+#' 
+#' Given the output of \code{\link{msomData}}, get the type of covariates (constant or mean of random variable, or sd of random variable).
+#' 
+#' @param staticDataUV the output of msomData, containing at least U or V element
+#' @param staticDataUV_sd the output of msomData, containing at least U or V element, where the covariates have the "_sd" suffix attached to their names, and these covariates correspond to similarly named (sans-suffix) elements of U and V in staticDataUV
+#' @param UV character of length 1, indicating whether to treat
+
+
+# ---- Split Covariates into Constants and Random Variables ----
+getCovType <- function(UV=c("U","V"), type=c("constant","mu","sd")){
+	
+	UV <- match.arg(UV)
+	
+	# Dimension Sizes, Number, and Names
+	dims <- dim(staticDataUV[[UV]])
+	nD <- length(dims)
+	dn <- dimnames(staticDataUV[[UV]])[[nD]]
+	dn_sd <- dimnames(staticDataUV[[UV]])[[nD]]
+	
+	# Get Names of Desired Covariates
+	covNames <- switch(type,
+		constant = dn[!dn%in%gsub("_sd","",dn_sd)],
+		mu = dn[dn%in%gsub("_sd","",dn_sd)],
+		sd = dn_sd
+	)
+	
+	# Get the Desired Covariates
+	covOut <- switch(type,
+		constant = staticDataUV[[UV]][,,covNames],
+		mu = staticDataUV[[UV]][,,covNames],
+		sd = staticDataUV_sd[[UV]][,,covNames]
+	)
+	
+	# Convert to Array, Get Dimension Information
+	covOut <- as.array(covOut)
+	cO_dims <- dim(covOut)
+	cO_nD <- length(cO_dims)
+	
+	# Make Sure covOut Dimensions match Full Cov Dimensions
+	if(cO_nD<nD){
+		# if here, I'm guessing it's because length(covNames) is 1
+		# and in that case, I want an array of the orignal major dims to be returned
+		stopifnot(length(covNames)==1)
+		
+		cO_newDim <- c(cO_dims,rep(1,nD-cO_nD)) 
+		covOut <- array(covOut, dim=cO_newDim, dimnames=c(dimnames(covOut),covNames))
+	}
+	
+	# Return
+	return(covOut)
+}
