7e770c4e10dff8b9957dcb5de2d448ab6a6366de
rBatt <battrd@gmail.com>
2015-12-13 20:02:59 -0500

    adjust data sizes again
    trying to save on the number of strata, and half species, almost all years

--- pkgBuild/test/msom_source_script.R
+++ pkgBuild/test/msom_source_script.R
@@ -19,15 +19,15 @@ library("rstan")
 # ebs.a2 <- ebs.a[,list(year=year, spp=spp, stratum=stratum, K=K, abund=abund, btemp=btemp, doy=yday(datetime))]
 #
 # # medium data set
-# set.seed(1337)
-# ind <- mpick(ebs.agg2, p=c(stratum=10, year=10), weight=TRUE, limit=60)
-# logic <- expression(
-# 	spp%in%spp[ind]
-# 	& stratum%in%stratum[ind]
-# 	& year%in%year[ind]
-# )
-# ebs.a1 <- ebs.agg2[eval(logic)][pick(spp, 20, w=FALSE)]
-# ebs.a2 <- ebs.a1[,list(year=year, spp=spp, stratum=stratum, K=K, abund=abund, btemp=btemp, stemp=stemp, depth=depth, doy=yday(datetime))]
+set.seed(1337)
+ind <- mpick(ebs.agg2, p=c(stratum=40, year=31), weight=TRUE, limit=60)
+logic <- expression(
+	spp%in%spp[ind]
+	& stratum%in%stratum[ind]
+	& year%in%year[ind]
+)
+ebs.a1 <- ebs.agg2[eval(logic)][pick(spp, 200, w=FALSE)]
+ebs.a2 <- ebs.a1[,list(year=year, spp=spp, stratum=stratum, K=K, abund=abund, btemp=btemp, stemp=stemp, depth=depth, doy=yday(datetime))]
 
 # # Medium-large data set
 # set.seed(1337)
@@ -35,7 +35,7 @@ library("rstan")
 # ebs.a2 <- ebs.a1[,list(year=year, spp=spp, stratum=stratum, K=K, abund=abund, btemp=btemp, stemp=stemp, depth=depth, doy=yday(datetime))]
 
 # largest data set
-ebs.a2 <- ebs.agg2[,list(year=year, spp=spp, stratum=stratum, K=K, abund=abund, btemp=btemp, stemp=stemp, depth=depth, doy=yday(datetime))]
+# ebs.a2 <- ebs.agg2[,list(year=year, spp=spp, stratum=stratum, K=K, abund=abund, btemp=btemp, stemp=stemp, depth=depth, doy=yday(datetime))]
 
 
 # ==================
@@ -76,7 +76,7 @@ mk_cov_rv_pow(ebs.a2, "depth", across="K", by=c("stratum","year"), pow=2)
 # = Cast Data for Stan =
 # ======================
 # ---- Get Basic Structure of MSOM Data Input ----
-stanData <- msomData(Data=ebs.a2, n0=50, cov.vars=cov.vars_use, u.form=~bt, v.form=~yr, valueName="abund", cov.by=c("year","stratum"))
+stanData <- msomData(Data=ebs.a2, n0=50, cov.vars=cov.vars_use, u.form=~bt+bt2+depth+depth^2+yr, v.form=~year, valueName="abund", cov.by=c("year","stratum"))
 
 stanData$nJ <- apply(stanData$nK, 1, function(x)sum(x>0)) # number of sites in each year
 stanData$X <- apply(stanData$X, c(1,2,4), function(x)sum(x)) # agg abund across samples
@@ -104,9 +104,7 @@ model_file <- "trawlDiversity/inst/stan/msomDynamic.stan"
 
 tag <- paste0("start_", format.Date(Sys.time(),"%Y-%m-%d_%H-%M-%S"))
 
-tag
-
-save.image(paste0("trawlDiversity/pkgBuild/test/msomDynamic_full_simple_EBS_preSave_4chain",tag,".RData"))
+save.image(paste0("trawlDiversity/pkgBuild/test/msomDynamic_40St_30Yr_200Spp_EBS_preSave_4chain",tag,".RData"))
 
 sessionInfo()
 
@@ -114,7 +112,7 @@ ebs_msom <- stan(
 	file=model_file, 
 	data=stanData, 
 	control=list(stepsize=0.01, adapt_delta=0.95, max_treedepth=15),
-	chains=4, iter=200, seed=1337, cores=4, verbose=F, refresh=1
+	chains=4, iter=150, seed=1337, cores=4, verbose=F, refresh=1
 )
 
-save.image(renameNow(paste0("trawlDiversity/pkgBuild/test/msomDynamic_full_simple_EBS_4chain",tag,"_end",".RData")), compress="xz")
+save.image(renameNow(paste0("trawlDiversity/pkgBuild/test/msomDynamic_40St_30Yr_200Spp_EBS_4chain",tag,"_end",".RData")), compress="xz")
