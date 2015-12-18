5d281db0a4bd323c08b9751aa62a6ed5227090ba
rBatt <battrd@gmail.com>
2015-12-18 18:18:39 -0500

    playing around trying to get to run; realized should be in source script

--- pkgBuild/msomDynamic.R
+++ pkgBuild/msomDynamic.R
@@ -14,18 +14,21 @@ library("trawlDiversity")
 # ebs.a2 <- ebs.a[,list(year=year, spp=spp, stratum=stratum, K=K, abund=abund, btemp=btemp, doy=yday(datetime))]
 
 # medium data set
-set.seed(1337)
-ind <- mpick(ebs.agg2, p=c(stratum=20, year=30), weight=TRUE, limit=60)
-logic <- expression(
-	spp%in%spp[ind]
-	& stratum%in%stratum[ind]
-	& year%in%year[ind]
-)
-ebs.a1 <- ebs.agg2[eval(logic)][pick(spp, 100, w=FALSE)]
-ebs.a2 <- ebs.a1[,list(year=year, spp=spp, stratum=stratum, K=K, abund=abund, btemp=btemp, stemp=stemp, doy=yday(datetime))]
+# set.seed(1337)
+# ind <- mpick(ebs.agg2, p=c(stratum=20, year=30), weight=TRUE, limit=60)
+# logic <- expression(
+# 	spp%in%spp[ind]
+# 	& stratum%in%stratum[ind]
+# 	& year%in%year[ind]
+# )
+# ebs.a1 <- ebs.agg2[eval(logic)][pick(spp, 100, w=FALSE)]
+# ebs.a2 <- ebs.a1[,list(year=year, spp=spp, stratum=stratum, K=K, abund=abund, btemp=btemp, stemp=stemp, doy=yday(datetime))]
 
 # largest data set
 # ebs.a2 <- ebs.agg2[,list(year=year, spp=spp, stratum=stratum, K=K, abund=abund, btemp=btemp, doy=yday(datetime))]
+
+ebs.a1 <- trim_msom("ebs", gridSize=1, plot=TRUE)
+ebs.a2 <- ebs.a1[,list(year=year, spp=spp, stratum=stratum, K=K, abund=abund, btemp=btemp, doy=yday(datetime))]
 
 
 # ======================================
@@ -33,12 +36,12 @@ ebs.a2 <- ebs.a1[,list(year=year, spp=spp, stratum=stratum, K=K, abund=abund, bt
 # ======================================
 # rename columns for shorthand
 setnames(ebs.a2, c("btemp"), c("bt"))
-setnames(ebs.a2, c("stemp"), c("st"))
+# setnames(ebs.a2, c("stemp"), c("st"))
 ebs.a2[,yr:=scale(as.integer(year))]
 
 # aggregate and transform (^2) btemp
 mk_cov_rv_pow(ebs.a2, "bt", across="K", by=c("stratum","year"), pow=2)
-mk_cov_rv_pow(ebs.a2, "st", across="K", by=c("stratum","year"), pow=2)
+# mk_cov_rv_pow(ebs.a2, "st", across="K", by=c("stratum","year"), pow=2)
 
 # scale and aggregate doy
 doy.mu <- ebs.a2[,mean(doy, na.rm=TRUE)]
@@ -52,7 +55,9 @@ mk_cov_rv(ebs.a2, "doy", across="K", by=c("stratum","year"))
 # ======================
 # ---- Get Basic Structure of MSOM Data Input ----
 # dynData <- msomData(Data=ebs.a2, n0=2, cov.vars=c(bt="bt",bt2="bt2",yr="yr", st="st", st2="st2", doy="doy"), u.form=~bt+bt2+st+yr, v.form=~year+doy, valueName="abund", cov.by=c("year","stratum"))
-dynData <- msomData(Data=ebs.a2, n0=2, cov.vars=c(bt="bt",bt2="bt2",yr="yr", doy="doy"), u.form=~bt+bt2, v.form=~doy+year, valueName="abund", cov.by=c("year","stratum"), v_rv=c("doy"))
+setkey(ebs.a2, year, stratum, K, spp)
+# dynData <- msomData(Data=ebs.a2, n0=2, cov.vars=c(bt="bt",bt2="bt2",yr="yr", doy="doy"), u.form=~bt+bt2, v.form=~doy+year, valueName="abund", cov.by=c("year","stratum"), u_rv=c("bt","bt2"),  v_rv=c("doy"))
+dynData <- msomData(Data=ebs.a2, n0=2, cov.vars=c(bt="bt",bt2="bt2",yr="yr", doy="doy"), u.form=~bt+bt2, v.form=~doy+year, valueName="abund", cov.by=c("year","stratum"),  v_rv=c("doy"))
 
 # ---- Add a counter for nJ (number of sites in each year) ----
 dynData$nJ <- apply(dynData$nK, 1, function(x)sum(x>0))
@@ -71,7 +76,7 @@ ebs_msom <- stan(
 	file=model_file, 
 	data=dynData, 
 	control=list(stepsize=0.01, adapt_delta=0.95, max_treedepth=15),
-	chains=4, iter=100, seed=1337, cores=4, verbose=F
+	chains=1, iter=50, seed=1337, cores=1, verbose=F
 )
 
 
