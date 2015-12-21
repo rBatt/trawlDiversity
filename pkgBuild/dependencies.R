8b5bb228ccad324cfada25c0f79616882e78ab33
rBatt <battrd@gmail.com>
2015-12-21 16:07:09 -0500

    update dependencies

--- pkgBuild/dependencies.R
+++ pkgBuild/dependencies.R
@@ -2,7 +2,14 @@
 update_dependencies <- function(){
 	devtools::use_package("data.table", type="Depends") # Basis for handling all data sets
 	devtools::use_package("trawlData", type="Depends") # Meets basic requirements for data content and format
+	
+	devtools::use_package("rbLib", type="Imports")
+	
+	devtools::use_package("R2jags", type="Suggests")
+	devtools::use_package("rstan", type="Suggests")
+	
 	devtools::use_package("reshape2", type="Suggests")
+	
 }
 
 
