4b2cc90a901a30069b2366dc761768a7c5abdb55
rBatt <battrd@gmail.com>
2015-12-21 19:17:47 -0500

    update run script for use with run_msom

--- pkgBuild/test/msom_source_script.R
+++ pkgBuild/test/msom_source_script.R
@@ -1,54 +1,19 @@
 
-
-
 # run on amphiprion: 
 # nohup R CMD BATCH -cwd --no-save trawlDiversity/pkgBuild/test/msom_source_script.R &
 
 # ========
 # = Load =
 # ========
-library("devtools")
-library("trawlData")
 library("trawlDiversity")
-library("rbLib")
-library("raster")
-library("rstan")
-library("R2jags")
-library("parallel")
-
-
-
-# ---- alternative approach to doing stan model, maybe ----
-# probably requires installing StanHeaders and rstan from source on development GitHub
-# msom_model <- stan_model(file=model_path, auto_write=TRUE)
-#
-# do_stan <- function(x, object, data, iter){
-# 	sampling(
-# 			object=object,
-# 			data=inputData,
-# 			control=stan_control,
-# 			pars= params_out,
-# 			chains=1, iter=iter, seed=1337, cores=1, verbose=F, refresh=1, chain_id=x
-# 	)
-# }
-#
-#
-# t_out <- sampling(
-# 		object=msom_model,
-# 		data=inputData,
-# 		control=stan_control,
-# 		pars= params_out,
-# 		chains=2, iter=4, seed=1337, cores=1, verbose=F, refresh=1
-# )
-#
-# out <- do_stan(1, msom_model, data=inputData, iter=iter)
-# # read1 <- read_stan_csv("ebs_msom_samples1")
-#
-# out <- mclapply(1:chains, do_stan, data=inputData, object=msom_model, iter=iter, mc.cores=cores, mc.preschedule=FALSE)
 
 sessionInfo()
 
+rm_out <- run_msom(
+	reg = "ebs", 
+	params_out = c("params"), 
+	# custom_params = c("Z", "Psi"), 
+	test=TRUE, n0=10, iter=200, pre_save=TRUE
+)
 
-
-
-save.image(renameNow(save_path), compress="xz")
+save.image(renameNow(rm_out[[2]]["save_path"]), compress="xz")
