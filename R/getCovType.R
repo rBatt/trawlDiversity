998abe52b724135d4fde445dd6020c80f9051d31
rBatt <battrd@gmail.com>
2016-01-04 15:20:48 -0500

    change to allow getCovType to handle length 1 dim
    other than just covariates (last dim)

--- R/getCovType.R
+++ R/getCovType.R
@@ -40,12 +40,26 @@ getCovType <- function(staticDataUV, staticDataUV_sd, UV=c("U","V"), type=c("con
 	
 	# Make Sure covOut Dimensions match Full Cov Dimensions
 	if(cO_nD<nD){
-		# if here, I'm guessing it's because length(covNames) is 1
+		# if here, it's b/c the length of one of the dims is 1
+		# (or if covNames is 1, which can be diff from corresp dim b/c of constant vs rv covs) 
 		# and in that case, I want an array of the orignal major dims to be returned
-		stopifnot(length(covNames)==1)
+		stopifnot(length(covNames)==1 | any(dims==1))
 		
+		# get a new dims, changing the last dim to be the number of covNames
+		dims2 <- dims
+		dims2[nD] <- length(covNames)
+		
+		# add back the original dimensions
 		cO_newDim <- c(cO_dims,rep(1,nD-cO_nD)) 
-		covOut <- array(covOut, dim=cO_newDim, dimnames=c(dimnames(covOut),covNames))
+		dimnames2 <- dimnames(staticDataUV[[UV]])
+		dimnames2[[nD]] <- covNames
+		dn_1 <- dimnames2[dims2==1]
+		covOut <- array(covOut, dim=cO_newDim, dimnames=c(dimnames(covOut),dn_1))
+		
+		# put dimensions back in right order
+		new_order <- order(c(which(dims2!=1), which(dims2==1)))
+		covOut <- aperm(covOut, new_order)
+		
 	}
 	
 	# Return
