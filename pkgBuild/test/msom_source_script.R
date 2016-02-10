f3ceefbd095618acb3a6c2d5b81665f047879725
rBatt <battrd@gmail.com>
2016-02-09 22:50:12 -0500

    prepare for amphip; tolFrac is 0.15, save files don't accumulate across regions

--- pkgBuild/test/msom_source_script.R
+++ pkgBuild/test/msom_source_script.R
@@ -2,7 +2,7 @@
 # run on amphiprion: 
 # nohup R CMD BATCH -cwd --no-save trawlDiversity/pkgBuild/test/msom_source_script.R &
 
-# nohup R CMD BATCH -cwd --no-save trawlDiversity/pkgBuild/test/msom_source_script.R msom_source_script_AllRegs_annual.Rout &
+# nohup R CMD BATCH -cwd --no-save trawlDiversity/pkgBuild/test/msom_source_script.R msom_source_script_AllRegs_annual_backwards.Rout &
 
 
 # ========
@@ -19,17 +19,24 @@ sessionInfo()
 
 n0_pad <- 100
 regs <- c("ebs", "ai", "goa", "wctri", "wcann", "gmex", "sa", "neus", "shelf", "newf")
-rm_out <- vector("list", length(regs))
+
 
 stan_folder <- file.path(system.file(package="trawlDiversity"), tolower("Stan"))
 model_location <- file.path(stan_folder, "msomStatic_norv.stan")
 compiled_stan_model <- stan_model(model_location)
 
-for(r in 1:length(regs)){
+# for(r in length(regs):1){
+# for(r in 1:2){ # ebs and ai
+for(r in 3:4){ # goa and wctri
+# for(r in 5:6){ # wcann and gmex
+# for(r in 7:8){ # sa and neus
+# for(r in 9:10){ # shelf and newf
+	
+	rm_out <- vector("list", length(regs)) # yes, this reset the contents of the list. Saving all regions together is too big
 	
 	t_reg <- regs[r]
 	
-	data_in_all0 <- trim_msom(t_reg, gridSize=0.5, depthStratum=100, tolFraction=0.25, grid_stratum=TRUE, plot=FALSE)
+	data_in_all0 <- trim_msom(t_reg, gridSize=0.5, depthStratum=100, tolFraction=0.15, grid_stratum=TRUE, plot=FALSE)
 
 	data_in_all <- data_in_all0
 	setkey(data_in_all, year, stratum, haulid, spp)
@@ -59,14 +66,14 @@ for(r in 1:length(regs)){
 			model_type = "Static", 
 			compiled_model = compiled_stan_model,
 			cores = 4, chains = 4,
-			test=FALSE, n0=annual_n0[i], iter=100, pre_save=FALSE, save_warmup=TRUE
+			test=FALSE, n0=annual_n0[i], iter=100, pre_save=FALSE, save_warmup=FALSE
 		), error=function(cond){message(paste("**Run failed for:", msg_progress));NA})
 		
 		cat("\n\n")
 		
 	}
 	
-	append_r <- paste0("_r1-", r, ".RData")
+	append_r <- paste0("_r", r, ".RData")
 	save_name <- gsub("\\.RData", append_r, rm_out[[r]][[i]][[3]]["save_path"])
 	save_name <- gsub("^\\./", "./trawlDiversity/pkgBuild/results/", save_name)
 	save(rm_out, file=save_name, compress="xz")
