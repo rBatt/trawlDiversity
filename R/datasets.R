6eb096f9d9771931e550d8898f8e0825c322efa8
rBatt <battrd@gmail.com>
2016-02-16 20:37:04 -0500

    add data set of 'suspicious' species in each region
    species that are more likely to have had increased detection than to have colonized

---
+++ R/datasets.R
@@ -0,0 +1,6 @@
+#' Species that have shown up late, and stayed around
+#' 
+#' Indicates species that are suspected of showing up in data set due to enhanced identification, not because of actual colonization of the region
+#' 
+#' @format data.table
+"show_up_spp"
