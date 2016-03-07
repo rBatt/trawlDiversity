1b26f0c0135cb9431304c306c7584163eb2f7c6e
rBatt <battrd@gmail.com>
2016-03-07 17:30:08 -0500

    add data_all dataset, and function to create it

---
+++ R/get_data_all.R
@@ -0,0 +1,33 @@
+#' Get Data from All Regions
+#' 
+#' Get data from all regions, trimmed and ready for MSOM
+#' 
+#' @param region Character indicating region abbreviation
+#' 
+#' @return a data.table
+#' 
+#' @export
+get_data_all <- function(region){
+	regs <- match.arg(region, c("ebs", "ai", "goa", "wctri", "wcann", "gmex", "sa", "neus", "shelf", "newf"), several.ok=TRUE)
+	pretty_reg <- c("ebs"="E. Bering Sea", "ai"="Aleutian Islands", "goa"="Gulf of Alaska", "wc"="West Coast US", "gmex"="Gulf of Mexico", "sa"="Southeast US", "neus"="Northeast US", "shelf"="Scotian Shelf", "newf"="Newfoundland")
+	pr <- names(pretty_reg)
+
+	names(pretty_col) <- pr
+
+
+	# ====================================
+	# = Get Trimmed Data for Each Region =
+	# ====================================
+	data_in_regs <- list()
+	for(r in 1:length(regs)){
+		t_reg <- regs[r]
+		t_trimmed <- trim_msom(t_reg, gridSize=0.5, depthStratum=100, tolFraction=0.15, grid_stratum=TRUE, plot=FALSE)
+		setkey(t_trimmed, year, stratum, haulid, spp)
+		data_in_regs[[regs[r]]] <- t_trimmed
+	}
+
+
+	data_all <- rbindlist(data_in_regs, fill=TRUE)
+
+	return(data_all)
+}
