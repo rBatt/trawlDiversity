133aa0b98c0e34719908b144c138ab0860fab84b
rBatt <battrd@gmail.com>
2016-03-10 16:43:21 -0500

    set up source script to run all regs, small iter
    few iterations, but with the intention of updating the models after. Also, smaller 0 species

--- pkgBuild/test/msom_source_script.R
+++ pkgBuild/test/msom_source_script.R
@@ -2,7 +2,7 @@
 # run on amphiprion: 
 # nohup R CMD BATCH -cwd --no-save trawlDiversity/pkgBuild/test/msom_source_script.R &
 
-# nohup R CMD BATCH -cwd --no-save trawlDiversity/pkgBuild/test/msom_source_script.R msom_source_script_AllRegs_annual_jags_30kIter_r9-10.Rout &
+# nohup R CMD BATCH -cwd --no-save trawlDiversity/pkgBuild/test/msom_source_script.R msom_source_script_AllRegs_annual_jags_6kIter_forUpdating_allRegs.Rout &
 
 
 # ========
@@ -17,7 +17,7 @@ library("R2jags")
 Sys.time()
 sessionInfo()
 
-n0_pad <- 100
+n0_pad <- 50
 regs <- c("ebs", "ai", "goa", "wctri", "wcann", "gmex", "sa", "neus", "shelf", "newf")
 
 
@@ -25,12 +25,12 @@ stan_folder <- file.path(system.file(package="trawlDiversity"), tolower("Stan"))
 model_location <- file.path(stan_folder, "msomStatic_norv_1yr.stan")
 compiled_stan_model <- stan_model(model_location)
 
-# for(r in length(regs):1){
-# for(r in 2:1){ # ebs and ai
+for(r in 1:length(regs)){
+# for(r in 1:2){ # ebs and ai
 # for(r in 3:4){ # goa and wctri
 # for(r in 5:6){ # wcann and gmex
 # for(r in 7:8){ # sa and neus
-for(r in 9:10){ # shelf and newf
+# for(r in 9:10){ # shelf and newf
 	
 	rm_out <- vector("list", length(regs)) # yes, this reset the contents of the list. Saving all regions together is too big
 	
@@ -66,15 +66,18 @@ for(r in 9:10){ # shelf and newf
 			model_type = "Static", 
 			compiled_model = compiled_stan_model,
 			cores = 4, chains = 4,
-			test=FALSE, n0=annual_n0[i], iter=30E3, pre_save=FALSE, save_warmup=FALSE
+			test=FALSE, n0=annual_n0[i], iter=6E3, pre_save=FALSE, save_warmup=FALSE
 		), error=function(cond){message(paste("**Run failed for:", msg_progress));NA})
 		
+		rm_out[[r]][[i]]$info['year'] <- u_yrs[i]
+		
+		print(Sys.time())
 		cat("\n\n")
 		
 	}
 	
 	append_r <- paste0("_r", r, ".RData")
-	save_name <- gsub("\\.RData", append_r, rm_out[[r]][[i]][[3]]["save_path"])
+	save_name <- gsub("\\.RData", append_r, rm_out[[r]][[1]][[3]]["save_path"])
 	save_name <- gsub("^\\./", "./trawlDiversity/pkgBuild/results/", save_name)
 	save(rm_out, file=save_name, compress="xz")
 	
