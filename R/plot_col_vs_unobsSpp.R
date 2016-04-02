248c4035e7aa0134c5caeb07a20658fe34ccac54
rBatt <battrd@gmail.com>
2016-04-01 22:17:07 -0400

    change margin sizes

--- R/plot_col_vs_unobsSpp.R
+++ R/plot_col_vs_unobsSpp.R
@@ -29,7 +29,7 @@ plot_col_vs_unobsSpp <- function(prn, Figures){
 	fig6_dim <- c(3.5, 3.5)
 	
 	dev.new(fig6_dim[1], fig6_dim[2])
-	
+	par(mar=c(1.75,1.75,0.2,0.2), cex=1, ps=8, mgp=c(0.85,0.1,0), tcl=-0.1)
 	processed[,plot(unobs_rich[-length(unobs_rich)], n_col[-1], xlab="Unobserved species present last year", ylab="Species colonizing this year")]
 	mtext(reg, side=3)
 	abline(a=0, b=1)
