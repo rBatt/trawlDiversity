c2adffa8898a69e2af9fee4eb7e9e4f9e7419f23
rBatt <battrd@gmail.com>
2016-04-04 17:59:00 -0400

    function to get response to temp/ depth combos

---
+++ pkgBuild/get_response.R
@@ -0,0 +1,59 @@
+alpha_unscale<-p[[1]]$alpha_unscale
+
+n_grid <- 20
+
+d_vals <- bt[,seq(min(depth), max(depth), length.out=n_grid)]
+b_vals <- bt[,seq(min(bt), max(bt), length.out=n_grid)]
+vals <- expand.grid(b=b_vals, d=d_vals)
+
+pred_resp <- function(b, d, alpha){
+	a <- matrix(alpha, nrow=5)
+	x <- as.matrix(cbind(1, b, b^2, d, d^2))
+	x%*%a
+}
+
+pr <- pred_resp(vals[,"b"], vals[,"d"], t(alpha_unscale[,list(a1,a2,a3,a4,a5)]))
+
+ac <- as.character
+spp_year0 <- alpha_unscale[,paste(spp,year),by=c("spp","year")]
+spp_year <- spp_year0[,V1]
+dn <- list(btemp=ac(b_vals),depth=ac(d_vals),iter=NULL, sppYear=spp_year)
+
+pr2 <- array(pr, dim=c(n_grid, n_grid, 800, ncol(pr)/800), dimnames=dn)
+pr3 <- apply(pr2, c(1,2,4), mean)
+
+
+spp_3 <- spp_year0[,spp]
+year_3 <- spp_year0[,year]
+
+u_spp <- unique(spp_3)
+
+for(i in 1:length(u_spp)){
+	t_spp <- u_spp[i]
+
+	sy_which <- which(spp_3==t_spp)
+
+	dev.new()
+	par(mfrow=auto.mfrow(length(sy_which)+1), oma=c(1,1,0.1,0.1))
+	
+	par(mar=c(0.1,0.1,1,0.1), ps=6, cex=1)
+	tsi <- sppImg(t_spp)
+	if(is.null(tsi)){plot(1, type="n")}
+
+	for(j in 1:length(sy_which)){
+		t_sy <- sy_which[j]
+		t_yr <- year_3[t_sy]
+		z <- plogis(pr3[,,t_sy])
+		x <- as.numeric(rownames(pr3))
+		y <- as.numeric(colnames(pr3))
+	
+		par(mar=c(2,2,1.75,0.1), ps=8, cex=1, mgp=c(0.5,0.1,0), tcl=-0.1)
+		fields::image.plot(z=z, x=x, y=y, main=t_yr, xlab="", ylab="")
+		par(mar=c(2,2,1.75,0.1), ps=8, cex=1, mgp=c(0.5,0.1,0), tcl=-0.1)
+		bt[(year)==t_yr,points(bt, depth)]
+
+	}
+	mtext("Depth (m)", side=1, outer=TRUE, line=0)
+	mtext("Temperature (C)", side=2, outer=TRUE, line=0)
+}
+
