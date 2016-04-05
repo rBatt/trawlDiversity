b6c57371a49ee6d9cdc174fd83ec2f5ad4f07ed6
rBatt <battrd@gmail.com>
2016-04-04 22:34:09 -0400

    shelf and newf run

--- pkgBuild/test/msom_source_script.R
+++ pkgBuild/test/msom_source_script.R
@@ -2,7 +2,7 @@
 # run on amphiprion: 
 # nohup R CMD BATCH -cwd --no-save trawlDiversity/pkgBuild/test/msom_source_script.R &
 
-# nohup R CMD BATCH -cwd --no-save trawlDiversity/pkgBuild/test/msom_source_script.R msom_source_script_AllRegs_annual_jags_noCulling_r8-8.Rout &
+# nohup R CMD BATCH -cwd --no-save trawlDiversity/pkgBuild/test/msom_source_script.R msom_source_script_AllRegs_annual_jags_noCulling_r9-10.Rout &
 
 
 # amphiprion process id's
@@ -10,7 +10,7 @@
 # ai & goa: 2705
 # wctri & wcann: 2795
 # gmex & sa: 2884
-# neus:
+# neus: 2973
 # shelf & newf:
 
 
@@ -83,8 +83,8 @@ if(lang_used=="Stan"){
 # for(r in 2:3){ # ai & goa
 # for(r in 4:5){ # wctri & wcann
 # for(r in 6:7){ # gmex & sa
-for(r in 8:8){ # neus
-# for(r in 9:10){ # shelf and newf
+# for(r in 8:8){ # neus
+for(r in 9:10){ # shelf and newf
 
 	rm_out <- vector("list", length(regs)) # yes, this reset the contents of the list. Saving all regions together is too big
 	
