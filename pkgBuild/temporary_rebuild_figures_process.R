a2e9cb12be4fb4ff6afaa0afacb246bdf0b2f66a
rBatt <battrd@gmail.com>
2016-04-09 14:06:39 -0400

    fix directory

--- pkgBuild/temporary_rebuild_figures_process.R
+++ pkgBuild/temporary_rebuild_figures_process.R
@@ -1,6 +1,6 @@
 # nohup R CMD BATCH -cwd --no-save trawlDiversity/pkgBuild/temporary_rebuild_figures_process.R temporary_rebuild_figures_process.Rout &
 
 setwd("~/Documents/School&Work/pinskyPost/trawl")
-source("trawlDiversity/test/process_msom_script.R")
-source("trawlDiversity/test/process_msom_figures.R")
+source("trawlDiversity/pkgBuild/test/process_msom_script.R")
+source("trawlDiversity/pkgBuild/test/process_msom_figures.R")
 
