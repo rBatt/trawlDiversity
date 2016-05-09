0b0efb18832ed3d85d856724b1b7a47d8a7d0936
rBatt <battrd@gmail.com>
2016-05-08 20:54:33 -0400

    add doc, trim out flab

--- R/timeSlope.R
+++ R/timeSlope.R
@@ -1,25 +1,34 @@
-# The fruit of: https://github.com/rBatt/trawl/commit/a04d8e166d7093538c698368d48542a0e60bd16c
-timeSlope<- function(x, ...){
-	# Old function
-	# if(sum(!is.na(x))<3){
-	# 	NA
-	# }else{
-	# 	as.numeric(lm(x~I(0:(length(x)-1)))$coef[2])
-	# }
-	nona <- !is.na(x)
+#' Time Slope
+#' 
+#' Calculate the temporal trendline (quickly)
+#' 
+#' @param x temporal predictor variable
+#' @param y response variable
+#' @param ... others arguments; not used
+#' 
+#' @details
+#' Requires at least 3 non-na values. 
+#' 
+#' For benchmarking, see: https://github.com/rBatt/trawl/commit/a04d8e166d7093538c698368d48542a0e60bd16c
+#' 
+#' @export
+timeSlope<- function(x, y, ...){
+	stopifnot(all(!is.na(x)))
+	nona <- !is.na(y)
+	
 	if(sum(nona)<3){
 		NA
 	}else{
-		lx <- length(x)
-		if(lx>=4E3){
-			tvec <- (1:lx)[nona]
-			x2 <- x[nona]
-			cor(x2, tvec)*(sd(x2)/sd(tvec))
+		ly <- length(y)
+		if(ly>=4E3){
+			tvec <- x[nona]
+			y2 <- y[nona]
+			cor(y2, tvec)*(sd(y2)/sd(tvec))
 
 		}else{
-			tvec <- cbind(1,1:lx)[nona,] # the 1 is a dummy vector for the intercept
+			tvec <- cbind(1,x)[nona,] # the 1 is a dummy vector for the intercept
 			ttvec <- t(tvec)
-			(solve(ttvec%*%tvec)%*%ttvec%*%x[nona])[2]
+			(solve(ttvec%*%tvec)%*%ttvec%*%y[nona])[2]
 		}
 	}
 }
