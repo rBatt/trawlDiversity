7e7aba76c93a79d845a01ac80da73e52153bd28a
rBatt <battrd@gmail.com>
2016-06-13 14:59:05 -0400

    order and save 4 manuscript figures

--- pkgBuild/manuscript_figures.R
+++ pkgBuild/manuscript_figures.R
@@ -14,23 +14,34 @@ names(pretty_col) <- names(pretty_reg)
 # = Manuscript Main Text Candidates =
 # ===================================
 # ---- richness time series in 1 panel ----
-dev.new(width=3.5, height=5)
+# dev.new(width=3.5, height=5)
+# pdf("~/Desktop/Figure1_richness_ts.pdf", width=3.5, height=5)
+png("~/Desktop/Figure1_richness_ts.png", width=3.5, height=5, units='in', res=200)
 par(mar=c(1.75,1.5,0.25,0.25),mgp=c(0.85,0.1,0), tcl=-0.1, cex=1, ps=8)
 comm_master[,plot(year, reg_rich, col=pretty_col[reg], xlab="Year", ylab="Estimated Richness", pch=20)]
 comm_master[,lines(year, reg_rich, lwd=0.5, col=adjustcolor(pretty_col[reg], 0.5)),by='reg']
 comm_master[,lines(year,fitted(lm(reg_rich~year))),by='reg']
-comm_master[,legend("topleft",ncol=1,legend=pretty_reg[una(reg)],text.col=pretty_col[una(reg)], inset=c(-0.085,-0.01))]
+comm_master[,legend("topleft",ncol=1,legend=pretty_reg[una(reg)],text.col=pretty_col[una(reg)], inset=c(-0.085,-0.01), bty='n')]
+dev.off()
 
-# ---- richness vs detectability in 1 panel ----
-dev.new(width=3.5, height=3.5)
-par(mar=c(1.75,1.5,0.25,0.25),mgp=c(0.85,0.1,0), tcl=-0.1, cex=1, ps=8)
-comm_master[,plot(plogis(detect_mu_avg), reg_rich, col=adjustcolor(pretty_col[reg],0.5), xlab="Detectability", ylab="Estimated Richness", pch=16)]
-comm_master[,lines(plogis(detect_mu_avg),fitted(lm(reg_rich~plogis(detect_mu_avg)))),by='reg']
-comm_master[,legend("topright",ncol=2,legend=pretty_reg[una(reg)],text.col=pretty_col[una(reg)], inset=c(-0.02, -0.02), bty='n')]
+# ---- prevalence vs time to absence ----
+# dev.new(width=3.5, height=3.5)
+# pdf("~/Desktop/Figure2_prevalence_absenceTime.pdf", width=3.5, height=3.5)
+png("~/Desktop/Figure2_prevalence_absenceTime.png", width=3.5, height=3.5, units='in', res=200)
+avg_prev_abs <- spp_master[!is.na(ext_dist_sign) & ext_dist_sign!=0,list(prevalence=mean(propStrata)),by=c("reg","ext_dist_sign","stretch_type")]
+par(mar=c(1.5,1.5,0.25,0.25), oma=c(0.1,0.1,0.1,0.1), cex=1, ps=8, mgp=c(0.75,0.1,0), tcl=-0.1, ylbias=0.35)
+avg_prev_abs[,plot(ext_dist_sign, prevalence, col=adjustcolor(pretty_col[(reg)], 0.5), pch=16, xlab="Years until extinction", ylab="Prevalence")]
+avg_prev_abs[,j={
+	lines(ext_dist_sign, fitted(lm(prevalence~ext_dist_sign)), col=pretty_col[(reg[1])], lwd=1.5)
+},by=c('reg',"stretch_type")]
+comm_master[,legend("topleft",ncol=2,legend=pretty_reg[una(reg)],text.col=pretty_col[una(reg)], inset=c(-0.075, -0.02), bty='n')]
+dev.off()
 
 # ---- colonization rate map ----
+# dev.new(height=3, width=7)
+# pdf("~/Desktop/Figure3_cRate_map.pdf", width=7, height=3)
+png("~/Desktop/Figure3_cRate_map.png", width=7, height=3, units='in', res=200)
 map_layout <- trawl_layout()
-dev.new(height=3, width=7)
 par(mar=c(1.5,1.5,0.5,0.5), mgp=c(0.75,0.1,0), tcl=-0.1,ps=8, cex=1, oma=c(0.5,0.5,1,0.1))
 layout(map_layout)
 u_regs <- mapDat[,unique(reg)]
@@ -41,8 +52,24 @@ for(r in 1:lu(u_regs)){
 mtext(bquote(Specific~Colonization~Rate~(C~~y^-1~spp^-1)), side=3, outer=TRUE, font=2, line=-0.5)
 mtext(bquote(Longitude~(phantom()*degree*E)), side=1, line=-0.4, outer=TRUE)
 mtext(bquote(Latitude~(phantom()*degree*N)), side=2, line=-0.75, outer=TRUE)
+dev.off()
 
-# ---- prevalence vs time to absence ----
+# ---- richness vs detectability in 1 panel ----
+# dev.new(width=3.5, height=3.5)
+# pdf("~/Desktop/Figure4_rich_detect.pdf", width=3.5, height=3.5)
+png("~/Desktop/Figure4_rich_detect.png", width=3.5, height=3.5, units='in', res=200)
+par(mar=c(1.75,1.5,0.25,0.25),mgp=c(0.85,0.1,0), tcl=-0.1, cex=1, ps=8)
+comm_master[,plot(plogis(detect_mu_avg), reg_rich, col=adjustcolor(pretty_col[reg],0.5), xlab="Detectability", ylab="Estimated Richness", pch=16)]
+comm_master[,lines(plogis(detect_mu_avg),fitted(lm(reg_rich~plogis(detect_mu_avg)))),by='reg']
+comm_master[,legend("topright",ncol=2,legend=pretty_reg[una(reg)],text.col=pretty_col[una(reg)], inset=c(-0.02, -0.02), bty='n')]
+dev.off()
+
+
+
+
+
+
+# ---- prevalence vs time to absence (panel per region, non-average) ----
 dev.new(width=3.5, height=3.5)
 par(mfrow=c(3,3), mar=c(0.75,0.75,0.25,0.25), oma=c(0.75,0.75,0.1,0.1), cex=1, ps=8, mgp=c(0.05,0.01,0), tcl=-0.1, ylbias=0.35)
 spp_master <- copy(trawlDiversity::spp_master)
@@ -71,15 +98,3 @@ spp_master[!is.na(stretch_type) & is.finite(ext_dist_sign2) & now_ext!=1,j={
 mtext("Years after extinction", side=1, line=0, outer=TRUE)
 mtext("Prevalence", side=2, line=-0.05, outer=TRUE)
 spp_master[,ext_dist_sign2:=NULL]
-
-
-# ---- average version of prevalence vs time to absence ----
-
-avg_prev_abs <- spp_master[!is.na(ext_dist_sign) & ext_dist_sign!=0,list(prevalence=mean(propStrata)),by=c("reg","ext_dist_sign","stretch_type")]
-dev.new(width=3.5, height=3.5)
-par(mar=c(1.5,1.5,0.25,0.25), oma=c(0.1,0.1,0.1,0.1), cex=1, ps=8, mgp=c(0.75,0.1,0), tcl=-0.1, ylbias=0.35)
-avg_prev_abs[,plot(ext_dist_sign, prevalence, col=adjustcolor(pretty_col[(reg)], 0.5), pch=16, xlab="Years until extinction", ylab="Prevalence")]
-avg_prev_abs[,j={
-	lines(ext_dist_sign, fitted(lm(prevalence~ext_dist_sign)), col=pretty_col[(reg[1])], lwd=1.5)
-},by=c('reg',"stretch_type")]
-comm_master[,legend("topleft",ncol=2,legend=pretty_reg[una(reg)],text.col=pretty_col[una(reg)], inset=c(-0.075, -0.02), bty='n')]
