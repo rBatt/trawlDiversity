5cd50e12e42afe5411e53b3804b9259f4055e5bf
rBatt <battrd@gmail.com>
2016-07-12 16:49:47 -0400

    add more dependencies to reduce notes warnings in check

--- pkgBuild/dependencies.R
+++ pkgBuild/dependencies.R
@@ -4,28 +4,28 @@ update_dependencies <- function(){
 	devtools::use_package("trawlData", type="Depends") # Meets basic requirements for data content and format
 	
 	devtools::use_package("rbLib", type="Imports")
-	
-	devtools::use_package("R2jags", type="Suggests")
-	
-	devtools::use_package("rstan", type="Suggests")
-	
+	devtools::use_package("stats", type="Imports")
+	devtools::use_package("methods", type="Imports")
+
+	devtools::use_package("raster", type="Suggests")
+	devtools::use_package("fields", type="Suggests")
 	devtools::use_package("reshape2", type="Suggests")
 	
+	devtools::use_package("R2jags", type="Suggests")
+	devtools::use_package("rstan", type="Suggests")
+	
 	devtools::use_package("vegan", type="Suggests")
-	
 	devtools::use_package("ade4", type="Suggests")
-	
 	devtools::use_package("zoo", type="Suggests")
 	
 	devtools::use_package("spatstat", type="Suggests")
-	
 	devtools::use_package("spdep", type="Suggests")
-	
-	devtools::use_package("raster", type="Suggests")
-	
 	devtools::use_package("Kendall", type="Suggests")
-	
 	devtools::use_package("forecast", type="Suggests")
+	
+	devtools::use_package("beanplot", type="Suggests")
+	
+	
 	
 }
 
