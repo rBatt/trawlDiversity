84ed9d88c52590ed0e7a17427a7c9d9f58874054
rBatt <battrd@gmail.com>
2016-08-08 11:11:12 -0400

    load trend statistics, don't calc here

--- pkgBuild/manuscript_stats.R
+++ pkgBuild/manuscript_stats.R
@@ -19,24 +19,9 @@ comm_master[,mean(reg_rich), by='reg'][reg%in%c("gmex","shelf")]
 comm_master[,stats::sd(reg_rich),by='reg']#[,mean(V1)]
 comm_master[,stats::sd(reg_rich),by='reg'][,mean(V1)]
 
-# ---- long-term trends ----
-# rich_trend_kendall <- comm_master[,cor.test(reg_rich, year, method="kendall")[c("estimate","p.value")], by='reg']
-rich_naive_trend_kendall <- comm_master[,tsTau(year, naive_rich), by='reg'] # comm_master[,Kendall::Kendall(year, reg_rich), by='reg']
-
-load("pkgBuild/results/processedMsom/p.RData")
-tau_list <- list()
-for(i in 1:length(p)){
-	cast_rich_iter <- as.matrix(dcast(p[[i]]$reg_rich_iter, iter~year, value.var="reg_rich")[,iter:=NULL])
-	rich_year <- as.integer(colnames(cast_rich_iter))
-	tau_list[[i]] <- c(list(reg=p[[i]]$reg_rich_iter[,unique(reg)]), as.list(post_trend(rich_year, cast_rich_iter, nSamp=1E2)))
-}
-rich_trend_kendall <- rbindlist(tau_list)
-
-rich_trend_kendall[,BH:=p.adjust(pvalue, method="BH")]
-rich_trend_kendall <- rich_trend_kendall[,list(reg=reg, estimate=tau, BH=BH, p.value=pvalue)]
-rich_naive_trend_kendall[,BH:=p.adjust(taup, method='BH')]
-rich_naive_trend_kendall <- rich_naive_trend_kendall[,list(reg=reg, estimate=tau, BH=BH, p.value=taup)]
-
+# ---- long-term trend ----
+load("pkgBuild/results/rich_trend_kendall.RData")
+load("pkgBuild/results/rich_naive_trend_kendall.RData")
 print(rich_naive_trend_kendall)
 print(rich_trend_kendall)
 
