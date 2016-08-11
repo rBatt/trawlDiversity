0d6f76fb83822349618972d0d8bc96bd68839b73
rBatt <battrd@gmail.com>
2016-08-10 22:55:24 -0400

    don't count wcann in p-value correction

--- pkgBuild/manuscript/manuscript_stats_richTrend.R
+++ pkgBuild/manuscript/manuscript_stats_richTrend.R
@@ -12,18 +12,16 @@ rich_naive_trend_kendall <- comm_master[,tsTau(year, naive_rich), by='reg'] # co
 
 load("pkgBuild/results/processedMsom/p.RData")
 tau_list <- list()
+pb <- txtProgressBar(min=0, max=length(p))
 for(i in 1:length(p)){
+	setTxtProgressBar(pb, i)
 	cast_rich_iter <- as.matrix(dcast(p[[i]]$reg_rich_iter, iter~year, value.var="reg_rich")[,iter:=NULL])
 	rich_year <- as.integer(colnames(cast_rich_iter))
 	tau_list[[i]] <- c(list(reg=p[[i]]$reg_rich_iter[,unique(reg)]), as.list(post_trend(rich_year, cast_rich_iter, nSamp=1E4)))
 }
 rich_trend_kendall <- rbindlist(tau_list)
 
-rich_trend_kendall[,BH:=p.adjust(pvalue, method="BH")]
-rich_trend_kendall <- rich_trend_kendall[,list(reg=reg, estimate=tau, BH=BH, p.value=pvalue)]
-rich_naive_trend_kendall[,BH:=p.adjust(taup, method='BH')]
-rich_naive_trend_kendall <- rich_naive_trend_kendall[,list(reg=reg, estimate=tau, BH=BH, p.value=taup)]
-
 save(rich_trend_kendall, file="pkgBuild/results/rich_trend_kendall.RData")
 save(rich_naive_trend_kendall, file="pkgBuild/results/rich_naive_trend_kendall.RData")
+
 
